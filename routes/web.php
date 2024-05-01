c127acf469b00fb3104045774ad73f77f5fc66b0
abdo <abdo.yahoo>
2024-05-01 12:28:26 +0300

    commit2

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Symfony\Component\Console\Input\Input;
+
+use function Laravel\Prompts\form;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,46 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return '<h1>welcome to laravel 10</h1>';
+// });
+// Route::get('/go', function () {
+//     return "<h1>go to laravel 10</h1>";
+// });
+// Route::get('/', function () {
+//     return '<form action="data" method="post">
+//     // <input type="hidden" name=" _token" value="' . csrf_token() . '"/>
+//     ' . csrf_field() . '
+//     <input type="submit" value="go"/></form>';
+// });
+// Route::post('/data', function () {
+//     return 'go';
+// // });
+
+//put
+
+
+// Route::get('/', function () {
+//     return '<form action="/my/data" method="post">
+//      <input type="hidden" name=" _method" value="put"/>
+//     ' . csrf_field() . '
+//     <input type="submit" value="go"/></form>';
+// });
+// Route::put('/my/data', function () {
+//     return 'welcome to put route';
+// });
+
+
+//patch
+
+// Route::get('/', function () {
+//     return '<form action="/my/patch" method="post">
+//     <input type="hidden" name= "_method" value="PATCH"/>
+//     ' . csrf_field() . '
+//     <input type= "submit" value="go"/></form>';
+// });
+// Route::patch('/my/patch', function () {
+//     return 'my patch route';
+// });
+
+//
